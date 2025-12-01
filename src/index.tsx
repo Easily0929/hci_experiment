be7389baf245b45bb80a8ec834d0a175ec11ae8c
Easily0929 <easily0929@gmail.com>
2025-12-01 14:59:55 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// 确保引入的文件名是 './App'，且你的 App.tsx 是默认导出 (export default)
 import App from './App'
-// 如果你有 index.css 可以在这里引入，没有也没关系
-import './style.css' 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// 先把 css 注释掉，防止因为找不到文件而白屏
+// import './style.css' 
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
