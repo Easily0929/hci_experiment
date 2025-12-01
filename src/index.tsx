de2294ec7a82ab845037c3544f3b34826d7c569d
Easily0929 <easily0929@gmail.com>
2025-12-01 15:58:35 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
-// 先把 css 注释掉，防止因为找不到文件而白屏
-// import './style.css' 
+import './style.css' 
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
