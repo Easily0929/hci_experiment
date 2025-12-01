5dbd0e87069e08f83b92b5f660286206c66031a4
Easily0929 <easily0929@gmail.com>
2025-12-01 15:59:38 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './style.css' 
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
+// 只要你的 index.html 里有 <script src="cdn.tailwindcss...">，样式就会生效
+// import './style.css'  <-- 除非你确定有这个文件，否则不要解开注释
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
